b5e621577137c29324abe0d29b5d86656277c3cf
javad <javad.feyzabadi@yahoo.com>
2023-03-04 15:14:03 +0330

    config

--- one.py
+++ one.py
@@ -1,7 +1,18 @@
 from celery import Celery
 from celery.utils.log import get_task_logger
 
-app = Celery('first',brocker = 'amqp://guest:guest@localhost:5672')
+app = Celery('first',brocker = 'amqp://guest:guest@localhost:5672',backend='rpc://')
+
+app.conf.update(
+    tasK_time_limit = 60 ,
+    task_soft_time_limit = 50 ,
+    worker_concurrency = 70 ,
+    worker_prefetch_multiplier=0 ,
+    task_ignore_result = True,
+    task_acks_late = False,
+)
+
+
 logger = get_task_logger(__name__)
 
 @app.task(name = 'one.disuing',bind = True,default_retry_delay = 600)
